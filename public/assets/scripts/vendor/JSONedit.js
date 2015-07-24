9f53e779b3d936872c4cc2a3b85ff593fbfcec72
Felipe Medina <fmedina@hugeinc.com>
2015-07-24 19:27:33 -0300

    +Cordova +Harplyss

---
+++ public/assets/scripts/vendor/JSONedit.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var app = angular.module('exampleApp', ['JSONedit']);
+
+function MainViewCtrl($scope, $filter) {
+
+    // example JSON
+    $scope.jsonData = jsonToEdit;
+
+    $scope.$watch('jsonData', function(json) {
+        $scope.jsonString = $filter('json')(json);
+    }, true);
+    $scope.$watch('jsonString', function(json) {
+        try {
+            $scope.jsonData = JSON.parse(json);
+            $scope.wellFormed = true;
+        } catch(e) {
+            $scope.wellFormed = false;
+        }
+    }, true);
+
+    $scope.undo = function() {
+        $scope.jsonString = JSON.stringify(jsonToUndo);
+        $scope.jsonData = jsonToUndo;
+        $scope.saveFile();                
+    }
+
+    $scope.saveFile = function() {
+        $.ajax({
+            url: window.location.href,
+            method: 'POST',
+            data: {
+                jsonFile: JSON.stringify($scope.jsonData)
+            },
+            dataType: 'text'
+        })
+        .done(function(data) {
+            $('.msg').show().text('Json file updated!').delay(2000).fadeOut(function() {
+                $(this).text('');
+            });
+        })
+        .fail(function(data) {
+            $('.msg').show().text('Could not save the file.').delay(2000).fadeOut(function() {
+                $(this).text('');
+            });
+        });
+    }
+}
